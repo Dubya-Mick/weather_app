ed48352e27426ac89385609641d9a2c2b9b799ba
Wyatt McMurry <wyattmcmurry1@gmail.com>
2021-04-07 07:26:45 -0400

    Began putting together the basic skeleton in html/css, beginning with mobile display, made sure the API call to openweather was at least correctly formatted

--- dist/main.js
+++ dist/main.js
@@ -3,7 +3,35 @@ var __webpack_exports__ = {};
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-alert('webpack is go!!!!!');
+
+const weatherFetcher = (() => {
+
+    let currentCityWeatherData = null;
+
+    const fetchCityWeather = async () => {
+        try {
+            const response = await fetch(
+                'https://api.openweathermap.org/data/2.5/weather?q=London&&units=metric&appid=9d8a24e77d28fa3feb28720f79b0b549',
+                {mode: 'cors'}
+            );
+            currentCityWeatherData = await response.json();
+            
+            console.log(currentCityWeatherData);
+        } catch (error) {
+            console.log(error);
+            alert('No weather found for that city');
+        }
+
+
+    }
+
+    return {
+        fetchCityWeather
+    }
+
+})();
+
+weatherFetcher.fetchCityWeather();
 /******/ })()
 ;
 //# sourceMappingURL=main.js.map
